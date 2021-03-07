589a9e7d1c710000af35ecb52044b738a42fe042
Curtis <ccombes@aol.com>
2021-03-07 17:15:27 -0500

    another test

--- ObjectBuilderTester/Form1.Designer.cs
+++ ObjectBuilderTester/Form1.Designer.cs
@@ -58,9 +58,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(608, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.Size = new System.Drawing.Size(97, 17);
             this.label1.TabIndex = 2;
-            this.label1.Text = "this is a test";
+            this.label1.Text = "blah blah blah";
             // 
             // Form1
             // 
